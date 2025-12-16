fbfc343951a9de03d35489d1479f506c6c3f697c
GranPatriarca02 <charliepsnoscar@gmail.com>
2025-12-16 20:28:36 +0100

    creación de la conexión a la base de datos

---
+++ database.py
@@ -0,0 +1,25 @@
+from peewee import *
+import envyte # type: ignore
+
+db = PostgresqlDatabase(
+    envyte.get("SUPABASE_DB_NAME"),
+    host=envyte.get("SUPABASE_DB_HOST"),
+    port=int(envyte.get("SUPABASE_DB_PORT")),
+    user=envyte.get("SUPABASE_DB_USER"),
+    password=envyte.get("SUPABASE_DB_PASSWORD")
+)
+def conectar_db():
+    """Conectar a la base de datos"""
+    try:
+        db.connect()
+        print(" Conectado a la base de datos")
+        return True
+    except Exception as e:
+        print(f" Error al conectar: {e}")
+        return False
+
+def cerrar_db():
+    """Cerrar la conexión a la base de datos"""
+    if not db.is_closed():
+        db.close()
+        print(" Desconectado de la base de datos")
