533e888499e54cd0337e55f85051d3458c875ec3
GranPatriarca02 <charliepsnoscar@gmail.com>
2025-12-18 21:00:38 +0100

    añadido crud de visitante y main temporal para probarlo

---
+++ main.py
@@ -0,0 +1,74 @@
+from database import conectar_db, cerrar_db
+from crear_tablas import crear_tablas
+from menu.funciones import *
+
+def mostrar_menu_principal():
+    """Mostrar menú principal"""
+    print("\n" + "="*50)
+    print("  SISTEMA DE GESTIÓN - PARQUE DE ATRACCIONES")
+    print("="*50)
+    print("\n GESTIÓN DE VISITANTES ")
+    print("1. Crear visitante")
+    print("2. Listar visitantes")
+    print("3. Eliminar visitante")
+    '''print("\n GESTIÓN DE ATRACCIONES ")
+    print("4. Crear atracción")
+    print("5. Listar atracciones")
+    print("6. Cambiar estado atracción")
+    print("7. Eliminar atracción")
+    print("\n GESTIÓN DE TICKETS ")
+    print("8. Crear ticket")
+    print("9. Listar tickets")
+    print("10. Marcar ticket como usado")
+    print("11. Listar tickets de un visitante")
+    print("12. Listar tickets de una atracción")
+    print("13. Visitantes con ticket para atracción")
+    print("\n CONSULTAS Y REPORTES ")
+    print("14. Consultas especializadas")
+    print("15. Modificaciones JSONB")
+    print("\n SISTEMA ")
+    print("0. Salir")
+    print("="*50)'''
+
+def main():
+    """Función principal"""
+    print("\n¡Bienvenido al Sistema de Gestión del Parque de Atracciones!")
+    
+    # Conectar a la base de datos
+    if not conectar_db():
+        print("No se pudo conectar a la base de datos. Saliendo...")
+        return
+    
+    # Crear tablas si no existen
+    print("\nVerificando tablas...")
+    crear_tablas()
+    fin = False
+    while not fin:
+        try:
+            mostrar_menu_principal()
+            opcion = validar_entero()
+            
+            if opcion == 0:
+                print("\n Sesión finalizada.")
+                fin = True
+            
+            elif opcion == 1:
+                crear_visitante_menu()
+            
+            elif opcion == 2:
+                listar_visitantes()
+            
+            elif opcion == 3:
+                eliminar_visitante_menu()
+            
+            
+        
+        except Exception as e:
+            print(f"\n Error inesperado: {e}")
+            input("\nPresione Enter para continuar...")
+    
+    # Cerrar conexión
+    cerrar_db()
+
+if __name__ == "__main__":
+    main()
--- menu/funciones.py
+++ menu/funciones.py
@@ -9,7 +9,7 @@ def validar_email(email):
     verificacion = '@' in email and '.' in email
     return verificacion
 
-def validar_entero(valor):
+def validar_entero():
     """Validar que el input sea un entero"""
     while True:
         try:
@@ -18,7 +18,7 @@ def validar_entero(valor):
         except ValueError:
             print("Error: Debe ingresar un número entero")
 
-def validar_float(valor):
+def validar_float():
     """Validar que el input sea un float"""
     while True:
         try:
@@ -35,3 +35,87 @@ def validar_fecha():
             return datetime.strptime(fecha, "%Y-%m-%d").date()
         except ValueError:
             print("Error: Formato de fecha inválido. Use YYYY-MM-DD")
+
+def crear_visitante_menu():
+    """Menú para crear un visitante y retorna el objeto creado"""
+    print(" CREAR VISITANTE")
+    nombre = input("Nombre: ")
+    valido = False
+    while not valido :
+        email = input("Email: ")
+        if validar_email(email):
+            valido = True
+        else:
+            print("Error: Email inválido")
+    
+    print("Altura en centímetros:")
+    altura = validar_entero()
+    
+    preferencias = None
+    valido = False
+    while not valido:
+        respuesta = input("¿Tienes alguna preferencia?(s/n)")
+        if respuesta.lower() == 's':
+            preferencias = {}
+            preferencias['tipo_favorito'] = input("Tipo favorito (extrema/familiar/infantil/acuatica): ")
+
+            restricciones = input("Restricciones (separadas por comas): ").split(',')
+            lista_restricciones_limpia = []
+
+            for restriccion in restricciones:
+                restriccion_limpia = restriccion.strip() # Quitamos espacios en blanco
+                if restriccion_limpia:         # Verificamos que no sea una cadena vacía
+                    lista_restricciones_limpia.append(restriccion_limpia)
+
+            preferencias['restricciones'] = lista_restricciones_limpia
+
+            preferencias['historial_visitas'] = []
+            visitante = VisitanteRepositorie.crear_visitante(nombre, email, altura, preferencias)
+            valido = True
+        elif respuesta.lower() == 'n':
+            print("Sin preferencias")
+            visitante = VisitanteRepositorie.crear_visitante(nombre, email, altura)
+            valido = True
+        else:
+            print("Opcion no valida")
+    if visitante:
+        print(f"Confirmacion: Visitante creado con ID: {visitante.id}")
+    else:
+        print("Error: No se pudo crear el visitante")
+        
+    return visitante
+
+def listar_visitantes():
+    """Listar todos los visitantes y retorna la lista"""
+    print("LISTA DE VISITANTES")
+    visitantes = VisitanteRepositorie.obtener_todos_visitantes()
+    
+    if not visitantes:
+        print("No hay visitantes registrados")
+        return []
+    
+    for v in visitantes:
+        print(f"\nID: {v.id}")
+        print(f"Nombre: {v.nombre}")
+        print(f"Email: {v.email}")
+        print(f"Altura: {v.altura} cm")
+        print(f"Fecha registro: {v.fecha_registro}")
+        if v.preferencias:
+            print(f"Preferencias: {json.dumps(v.preferencias, indent=2)}")
+            
+    return visitantes
+
+def eliminar_visitante_menu():
+    """Menú para eliminar un visitante y retorna True si fue exitoso"""
+    print("ELIMINAR VISITANTE")
+    print("ID del visitante:")
+    visitante_id = validar_entero()
+    
+    exito = VisitanteRepositorie.eliminar_visitante(visitante_id)
+    
+    if exito:
+        print("Confirmacion: Visitante eliminado exitosamente")
+    else:
+        print("Error: No se pudo eliminar al visitante")
+        
+    return exito
